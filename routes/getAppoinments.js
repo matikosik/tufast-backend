98c7ab76d99bb6c6785929b3f525ac78884c387a
IanShih2003 <54551203+IanShih2003@users.noreply.github.com>
2020-11-18 22:59:55 -0300

    Update getAppoinments.js

--- routes/getAppoinments.js
+++ routes/getAppoinments.js
@@ -4,30 +4,25 @@ var jwt = require("jsonwebtoken");
 
 const locals = require("../models/locals");
 const appointment = require("../models/appointment");
+const users = require("../models/users");
 
 router.post("/", async (req, res) => {
   const decoded = jwt.decode(req.headers.token);
 
   if (decoded.local == true) {
     const appointments = await appointment.find({
-      idLocal: decoded.user
+      idLocal: decoded.user,
     });
-    if (appointments) {
-      res.send(appointments);
+    for (var i = 0; i < appointments.length; i++) {
+      const userInfo = await users.findById(appointments[i].idUser);
+      appointments.userInfo = userInfo;
     }
-    else{
-        res.send("No hay turnos pendientes")
-    }
+    res.send(appointments);
   } else if (decoded.local == false) {
     const appointments = await appointment.find({
-      idUser: decoded.user
+      idUser: decoded.user,
     });
-    if (appointments) {
-      res.send(appointments);
-    }
-    else{
-        res.send("No hay turnos pendientes")
-    }
+    res.send(appointments);
   }
 });
 
