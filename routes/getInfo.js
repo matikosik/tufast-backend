3dcfae74d99c346b44dd9d629afdd6e8ba307c6b
IanShih2003 <54551203+IanShih2003@users.noreply.github.com>
2020-11-20 09:36:03 -0300

    getInfo 20/11

--- routes/getInfo.js
+++ routes/getInfo.js
@@ -1,30 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+
 const users = require("../models/users");
 const locals = require("../models/locals");
-
-const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   const decoded = jwt.decode(req.headers.token);
   if (decoded.local == false) {
     const user = await users.findById(decoded.user);
-    for (key in req.body) {
-      if (req.body[key] === null) {
-        req.body[key] = user[key];
-      }
-    }
-    await users.findByIdAndUpdate(decoded.user);
+    return res.send(user);
   }
+
   if (decoded.local == true) {
     const local = await locals.findById(decoded.user);
-    for (key in req.body) {
-      if (req.body[key] === null) {
-        req.body[key] = local[key];
-      }
-    }
-    await users.findByIdAndUpdate();
+    res.send(local);
   }
 });
 
