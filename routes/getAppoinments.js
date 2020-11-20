2b434c6c578779940e7ceafebe3d048e69162aeb
IanShih2003 <54551203+IanShih2003@users.noreply.github.com>
2020-11-20 01:03:38 -0300

    Update getAppoinments.js

--- routes/getAppoinments.js
+++ routes/getAppoinments.js
@@ -6,45 +6,148 @@ const appointment = require("../models/appointment");
 const locals = require("../models/locals");
 const users = require("../models/users");
 
-router.post("/", async (req, res) => {
+router.post("/confirmed", async (req, res) => {
   const decoded = jwt.decode(req.headers.token);
 
   if (decoded.local == true) {
-    var resultado = []
+    var resultado = [];
     const appointments = await appointment
       .find({
-        idLocal: decoded.user
+        idLocal: decoded.user,
+        status: "confirmed"
       })
       .lean();
     for (var i = 0; i < appointments.length; i++) {
-      var json = {}
-      const userInfo = await users.findById(appointments[i].idUser, {_id: 0, __v: 0}).lean();
-      for(key in userInfo){
-        json[key] = userInfo[key]
+      var json = {};
+      const userInfo = await users
+        .findById(appointments[i].idUser, { _id: 0, __v: 0 })
+        .lean();
+      for (key in userInfo) {
+        json[key] = userInfo[key];
       }
-      for(key in appointments[i]){
-        json[key] = appointments[i][key]
+      for (key in appointments[i]) {
+        json[key] = appointments[i][key];
       }
-      resultado.push(json)
+      resultado.push(json);
     }
     return res.send(resultado);
   } else if (decoded.local == false) {
-    var resultado = []
-    const appointments = await appointment.find({
-      idUser: decoded.user,
-    }).lean();
+    var resultado = [];
+    const appointments = await appointment
+      .find({
+        idUser: decoded.user,
+      })
+      .lean();
     for (var i = 0; i < appointments.length; i++) {
-      var json = {}
-      const localInfo = await locals.findById(appointments[i].idLocal, {_id: 0, __v: 0}).lean();
-      for(key in localInfo){
-        json[key] = localInfo[key]
+      var json = {};
+      const localInfo = await locals
+        .findById(appointments[i].idLocal, { _id: 0, __v: 0 })
+        .lean();
+      for (key in localInfo) {
+        json[key] = localInfo[key];
       }
-      for(key in appointments[i]){
-        json[key] = appointments[i][key]
+      for (key in appointments[i]) {
+        json[key] = appointments[i][key];
       }
-      resultado.push(json)
+      resultado.push(json);
     }
-    return res.send(resultado)
+    return res.send(resultado);
+  }
+});
+router.post("/pending", async (req, res) => {
+  const decoded = jwt.decode(req.headers.token);
+
+  if (decoded.local == true) {
+    var resultado = [];
+    const appointments = await appointment
+      .find({
+        idLocal: decoded.user,
+        status: "pending"
+      })
+      .lean();
+    for (var i = 0; i < appointments.length; i++) {
+      var json = {};
+      const userInfo = await users
+        .findById(appointments[i].idUser, { _id: 0, __v: 0 })
+        .lean();
+      for (key in userInfo) {
+        json[key] = userInfo[key];
+      }
+      for (key in appointments[i]) {
+        json[key] = appointments[i][key];
+      }
+      resultado.push(json);
+    }
+    return res.send(resultado);
+  } else if (decoded.local == false) {
+    var resultado = [];
+    const appointments = await appointment
+      .find({
+        idUser: decoded.user,
+      })
+      .lean();
+    for (var i = 0; i < appointments.length; i++) {
+      var json = {};
+      const localInfo = await locals
+        .findById(appointments[i].idLocal, { _id: 0, __v: 0 })
+        .lean();
+      for (key in localInfo) {
+        json[key] = localInfo[key];
+      }
+      for (key in appointments[i]) {
+        json[key] = appointments[i][key];
+      }
+      resultado.push(json);
+    }
+    return res.send(resultado);
+  }
+});
+router.post("/old", async (req, res) => {
+  const decoded = jwt.decode(req.headers.token);
+
+  if (decoded.local == true) {
+    var resultado = [];
+    const appointments = await appointment
+      .find({
+        idLocal: decoded.user,
+        status: "old"
+      })
+      .lean();
+    for (var i = 0; i < appointments.length; i++) {
+      var json = {};
+      const userInfo = await users
+        .findById(appointments[i].idUser, { _id: 0, __v: 0 })
+        .lean();
+      for (key in userInfo) {
+        json[key] = userInfo[key];
+      }
+      for (key in appointments[i]) {
+        json[key] = appointments[i][key];
+      }
+      resultado.push(json);
+    }
+    return res.send(resultado);
+  } else if (decoded.local == false) {
+    var resultado = [];
+    const appointments = await appointment
+      .find({
+        idUser: decoded.user,
+      })
+      .lean();
+    for (var i = 0; i < appointments.length; i++) {
+      var json = {};
+      const localInfo = await locals
+        .findById(appointments[i].idLocal, { _id: 0, __v: 0 })
+        .lean();
+      for (key in localInfo) {
+        json[key] = localInfo[key];
+      }
+      for (key in appointments[i]) {
+        json[key] = appointments[i][key];
+      }
+      resultado.push(json);
+    }
+    return res.send(resultado);
   }
 });
 
